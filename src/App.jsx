35cfc990fa4012310fa27ea5ab1833b94e2c92ab
woojung007 <eeeeve007@gmail.com>
2023-07-06 22:03:45 +0900

    project initial setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import MainPage from './pages/MainPage';
-import VideoDetailPage from 'pages/VideoDetailPage';
-
 function App() {
-    return (
-        <Routes>
-            <Route path='/' element={<MainPage />} />
-            <Route path='/:videoId' element={<VideoDetailPage />} />
-        </Routes>
-    );
+    return <> </>;
 }
 
 export default App;
