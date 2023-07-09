2664f34cb561b7337085990b3ce5d521e2b39221
woojung007 <eeeeve007@gmail.com>
2023-07-09 21:29:11 +0900

    feat: header styling with tailwind

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -18,19 +18,25 @@ export default function SearchHeader() {
     }, [keyword]);
 
     return (
-        <header>
+        <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
             {/* 
               navigate 와 div 대신에 Link 태그 사용해도 된다. 
               <Link to='/'>
             */}
-            <Link to='/'>
-                <BsYoutube />
-                <h1>Youtube</h1>
+            <Link to='/' className='flex items-center'>
+                <BsYoutube className='text-4xl text-brand' />
+                <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
             </Link>
 
-            <form onSubmit={handleSubmit}>
-                <input type='text' placeholder='Search...' value={text} onChange={(e) => setText(e.target.value)} />
-                <button>
+            <form className='w-full flex justify-center' onSubmit={handleSubmit}>
+                <input
+                    className='w-7/12 p-2 outline-none bg-black text-gray-50'
+                    type='text'
+                    placeholder='Search...'
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button className='bg-zinc-600 px-4'>
                     <BsSearch />
                 </button>
             </form>
