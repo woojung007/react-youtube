cbd72a404ae54e699884603c9cc17a856cd63d69
woojung007 <eeeeve007@gmail.com>
2023-07-16 16:26:25 +0900

    feat: youtube / fake client class

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,10 +1,14 @@
+import FakeYoutubeClient from 'api/fakeYoutubeClient';
 import Youtube from 'api/youtube';
+import YoutubeClient from 'api/youtubeClient';
 import { createContext, useContext } from 'react';
 
 export const YoutubeApiContext = createContext();
 
 export function YoutubeApiProvider({ children }) {
-    const youtube = new Youtube();
+    const client = new FakeYoutubeClient();
+    // const client = new YoutubeClient();
+    const youtube = new Youtube(client);
 
     return <YoutubeApiContext.Provider value={{ youtube }}>{children}</YoutubeApiContext.Provider>;
 }
