8182860f0e7e69250d2c5bc700586f4be494a318
woojung007 <eeeeve007@gmail.com>
2023-10-07 22:51:59 +0900

    🐛 fix formatAgo lang type

--- src/util/date.js
+++ src/util/date.js
@@ -3,6 +3,6 @@ import koLocale from 'timeago.js/lib/lang/ko';
 
 register('ko', koLocale);
 
-export const formatAgo = (date, lang = 'en_US') => {
+export const formatAgo = (date, lang = 'ko') => {
     return format(date, lang);
 };
