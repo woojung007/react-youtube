246b9f11367198d1f5cff1ff1404b1e1774bb371
woojung007 <eeeeve007@gmail.com>
2023-10-07 23:05:46 +0900

    ✅ add VideoCard navigate test

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import VideoCard from 'components/VideoCard';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { formatAgo } from 'util/date';
 
 describe('VideoCard', () => {
@@ -39,5 +40,26 @@ describe('VideoCard', () => {
         expect(screen.getByText(title)).toBeInTheDocument();
         expect(screen.getByText(channelTitle)).toBeInTheDocument();
         expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+    });
+
+    it('navigates to detailed video page with video state when clicked', () => {
+        // 전달된 상태를 보여주기 위해서 만든 임의의 테스트용 컴포넌트
+        function LocationStateDisplay() {
+            return <pre>{JSON.stringify(useLocation().state)}</pre>;
+        }
+
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <Routes>
+                    <Route path='/' element={<VideoCard video={video} />} />
+                    <Route path={`/videos/watch/${video.id}`} element={<LocationStateDisplay />} />
+                </Routes>
+            </MemoryRouter>,
+        );
+
+        const card = screen.getByRole('listitem'); // li 태그
+        userEvent.click(card); // list 카드가 클릭되면
+
+        expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
     });
 });
