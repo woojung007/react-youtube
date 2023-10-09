d57ac1be69b0634ac64ba6e240593e725a1439f2
woojung007 <eeeeve007@gmail.com>
2023-10-09 16:40:42 +0900

    ✅ add channelInfo test

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -7,9 +7,9 @@ export default function ChannelInfo({ id, name }) {
     const { data: url } = useQuery(['channel', id], () => youtube.channelImageUrl(id));
 
     return (
-        <div className='flex my-4 mb-8 items-center'>
+        <div className='flex items-center my-4 mb-8'>
             {url && <img className='w-10 h-10 rounded-full' src={url} alt={name} />}
-            <p className='text-lg font-medium ml-2'>{name}</p>
+            <p className='ml-2 text-lg font-medium'>{name}</p>
         </div>
     );
 }
--- src/components/tests/ChannelInfo.test.js
+++ src/components/tests/ChannelInfo.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable testing-library/prefer-find-by */
+import '@testing-library/jest-dom';
 import { render, screen, waitFor } from '@testing-library/react';
-import { default as ChannelInfo } from 'components/ChannelInfo';
+import ChannelInfo from 'components/ChannelInfo';
 import { Route } from 'react-router-dom';
 import { withAllContexts, withRouter } from 'tests/utils';
 
@@ -13,7 +15,41 @@ describe('ChannelInfo', () => {
         fakeYoutube.channelImageUrl.mockReset();
     });
 
+    /**
+     * 1. snapshot(img, p)
+     * 2. url X
+     * (3. url O)
+     */
+
     it('renders correctly', async () => {
+        fakeYoutube.channelImageUrl.mockImplementation(() => 'url');
+
+        const { asFragment } = render(
+            withAllContexts(
+                withRouter(<Route path='/' element={<ChannelInfo id='id' name='channel' />} />),
+                fakeYoutube,
+            ),
+        );
+
+        await waitFor(() => expect(screen.getByRole('img')));
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('renders without URL', () => {
+        fakeYoutube.channelImageUrl.mockImplementation(() => {
+            throw new Error('error');
+        });
+
+        render(
+            withAllContexts(withRouter(<Route path='/' element={<ChannelInfo id='id' name='channel' />} />)),
+            fakeYoutube,
+        );
+
+        expect(screen.queryByRole('img')).toBeNull();
+    });
+
+    it('renders with URL', async () => {
         fakeYoutube.channelImageUrl.mockImplementation(() => 'url');
 
         render(
@@ -23,6 +59,6 @@ describe('ChannelInfo', () => {
             ),
         );
 
-        await waitFor(() => expect(screen.getByText('channel')));
+        await waitFor(() => expect(screen.getByRole('img')).toBeInTheDocument());
     });
 });
